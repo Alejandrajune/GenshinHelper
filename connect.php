e157f6fec8ea98b014baf63eb2f526f1efb175d6
Alejandrajune <72174292+Alejandrajune@users.noreply.github.com>
2021-12-04 18:01:18 -0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,13 +7,14 @@
 -->
 
 <?php
-$user = 'Application';
-$password = 'GenshinApp!';
+$user = 'root';
+$password = 'Paimon6879!';
 $db = 'genshinhelper';
 $host = '127.0.0.1';
 $port = '3306';
 
 $link = mysqli_init();
+mysqli_ssl_set($link, "certs/client-key.pem", "certs/client-cert.pem", "certs/ca-cert.pem", NULL, NULL);
 $success = mysqli_real_connect(
     $link,
     $host,
